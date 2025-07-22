0b57d6b66d918ee1f558e88cb146f984a336a31c
Kim Robinson <kimmykokonut@gmail.com>
2025-07-22 16:27:34 -0700

    remove archive urls

--- src/urls.py
+++ src/urls.py
@@ -3,49 +3,31 @@ home_urls = {
 }
 
 about_and_info_urls = {
-    # from old site
-    # "https://www.capeperpetuacollaborative.org/the-team": "about-us-team",
     "https://www.capeperpetuacollaborative.org/team": "about-team",
-    # "https://www.capeperpetuacollaborative.org/partners": "about-us-partners",
     "https://www.capeperpetuacollaborative.org/sponsors-and-partners": "about-partners",
-    # "https://www.capeperpetuacollaborative.org/our-fiscal-sponsor": "about-us-fiscal",
     "https://www.capeperpetuacollaborative.org/behind-our-logo": "about-logo",
-    # "https://www.capeperpetuacollaborative.org/our-impact": "about-us-impact",
-    # "https://www.capeperpetuacollaborative.org/visit": "visit",
     "https://www.capeperpetuacollaborative.org/what-to-know": "visit-know",
     "https://www.capeperpetuacollaborative.org/things-to-do": "visit-do",
-    # "https://www.capeperpetuacollaborative.org/learn": "learn",
     "https://www.capeperpetuacollaborative.org/tribes-of-the-area": "about-tribes",
     "https://www.capeperpetuacollaborative.org/commitments-and-action": "about-action",
 }
 
 programs_urls = {
     "https://www.capeperpetuacollaborative.org/events-calendar": "events-calendar",
-    # "https://www.capeperpetuacollaborative.org/programs": "programs",
-    # "https://www.capeperpetuacollaborative.org/tidepool-ambassador": "programs-tidepool",
     "https://www.capeperpetuacollaborative.org/tidepool-tours": "events-tidepool",
-    # "https://www.capeperpetuacollaborative.org/marine-reserve-ambassador": "programs-marine-ambassador",
     "https://www.capeperpetuacollaborative.org/marine-reserve-education": "events-marine-education",
-    # "https://www.capeperpetuacollaborative.org/bioblitzes": "programs-bioblitzes",
     "https://www.capeperpetuacollaborative.org/bioblitz": "events-bioblitz",
-    # "https://www.capeperpetuacollaborative.org/sea-star-monitoring": "programs-seastar",
-    # "https://www.capeperpetuacollaborative.org/marine-debris-surveys": "programs-debris",
-    # "https://www.capeperpetuacollaborative.org/youngwavemakers": "programs-wavemakers",
     "https://www.capeperpetuacollaborative.org/wavemakers": "get-involved-wavemakers",
-    # "https://www.capeperpetuacollaborative.org/speaker-series": "programs-speakers",
     "https://www.capeperpetuacollaborative.org/speaker-series": "events-speaker",
     "https://www.capeperpetuacollaborative.org/land-sea-symposium": "events-land-sea",
-    # "https://www.capeperpetuacollaborative.org/summerinternships": "programs-internships",
     "https://www.capeperpetuacollaborative.org/summer-internship": "get-involved-internships",
 }
 
 forms_urls = {
-    # "https://docs.google.com/forms/d/e/1FAIpQLSfmkJB79Q3gqPHh0zeLA3sLp22s6qqpJv1fprlg0URD4QObZA/viewform": "volunteer",
     "https://www.capeperpetuacollaborative.org/volunteer": "get-involved-volunteer",
-    # "https://www.capeperpetuacollaborative.org/contact-1": "contact",
     "https://www.capeperpetuacollaborative.org/contact-us": "about-contact",
+    # 3rd party links unchanged from archived site.
     # "https://www.discovernw.org/store_cape-perpetua-collaborative_8701-671420b.html": "donate",
     "https://www.capeperpetuacollaborative.org/donate": "get-involved-donate",
     # "https://facebook.us18.list-manage.com/subscribe?u=c8bd8a1b0ff8af8ef5d75e878&id=24b2f4f5fd": "newsletter",
-    # where new newsletter?
 }
